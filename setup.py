6214f6bdc62fa06fb5d0a964106847eb77cad820
Sharan <62549774+A-Sharan1@users.noreply.github.com>
2024-06-05 15:52:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='my_project_manager',
-    version='0.1',
+    name='ProjectSystemManager',
+    version='0.4',
     packages=find_packages(),
     install_requires=[
-        'configparser',  # For parsing configuration files
-        # Add any other dependencies you're using here
-    ],
+        'configparser',
+    ],  # Add a comma here
+    author='A. Sharan',
+    author_email='hemsharan3050@gmail.com',
+    description='ProjectSystemManager is a Python library designed to manage projects by creating, updating, searching, and fetching project details. It allows you to organize projects in a specified directory and store relevant information such as project name, creation date, technologies used, and a summary.',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+     project_urls={
+           'Source Repository': 'https://github.com/A-Sharan1/ProjectManager/'
+    }
 )
